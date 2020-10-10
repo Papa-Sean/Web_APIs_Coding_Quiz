7a16daf9cd232e6bce9dcabb81f08fd48416cb24
Papa-Sean <smsugrue@gmail.com>
2020-10-09 20:47:04 -0400

    event listener

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 const quizContainer = document.getElementById("quiz-container");
 const resultsContainer = document.getElementById("results-container");
 const submitButton = document.getElementById("submit");
+
+function buildQuiz(){}
+function showResults(){}
+
+buildQuiz();
+//add event listener for the button
+submitButton.addEventListener('click', showResults);
