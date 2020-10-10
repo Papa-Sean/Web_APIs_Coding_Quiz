941b6a198dd28ce139d970e380f07b2bd6b38bc4
Papa-Sean <smsugrue@gmail.com>
2020-10-09 20:44:30 -0400

    linked to html doc

---
+++ script.js
@@ -0,0 +1,4 @@
+// linked my quiz/results/button to html doc
+const quizContainer = document.getElementById("quiz-container");
+const resultsContainer = document.getElementById("results-container");
+const submitButton = document.getElementById("submit");
